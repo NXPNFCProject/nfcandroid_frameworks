f5f3abb22b65aacabc1025830311fca3f2d24034
Bhupendra Pawar <bhupendra.pawar@nxp.com>
2020-02-12 10:38:18 +0100

    Revert "[artf709748][OSU]: Initial OSU JAR Version"
    This reverts commit cfe5dc9f9f1c768825b47f38466bf3fd63e33af4.
    
    Change-Id: I21d7121c34d0cb538576a636f768ca103c96f679

--- osu/src/com/nxp/id/tp/common/coding/asn1/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * ASN.1 syntax. Provides convenient
- * hex string intfs speaking the "same language" as the other library fct.
- * Coding rule is limited to DER.
- *
- * @author nxp72467 <klaus.potzmader@nxp.com>
- */
-package com.nxp.id.tp.common.coding.asn1;
