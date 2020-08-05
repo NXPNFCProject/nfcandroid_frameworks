496a7e371b212a8ba8f7eb831917b32863f15c3e
Hariprasad Nalacheruvu <nxp79939@lsv03234.swis.in-blr01.nxp.com>
2020-08-05 20:56:02 +0530

    Change SEMS OMAPI terminal mapping by name instead of index
    To avoid wrong terminal/interface with index(0) which is varying
    instead using fixed terminal name as eSE1

--- sems/com/nxp/sems/SemsAgent.java
+++ sems/com/nxp/sems/SemsAgent.java
@@ -40,7 +40,7 @@ public final class SemsAgent {
   public static final byte SEMS_STATUS_DENIED = 0x03;
   public static final byte SEMS_STATUS_UNKNOWN = 0x0F;
   public static final short major = 0;
-  public static final short minor = 9;
+  public static final short minor = 10;
 
   private static SemsAgent sInstance;
   private static Context sContext = null;
